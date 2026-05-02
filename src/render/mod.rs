627362497cafb0055136596dd2dca3fb76316359
Max Kasperowicz <yet3.dev@gmail.com>
2026-05-02 13:23:12 +0200

    feat: add option to set the size of the quite zone

--- src/render/mod.rs
+++ src/render/mod.rs
@@ -108,6 +108,12 @@ impl<'a, P: Pixel> Renderer<'a, P> {
         self
     }
 
+    /// Sets the size of the quite zone.
+    pub fn quiet_zone_size(&mut self, quiet_zone: u32) -> &mut Self {
+        self.quiet_zone = quiet_zone;
+        self
+    }
+
     /// Whether to include the quiet zone in the generated image.
     pub fn quiet_zone(&mut self, has_quiet_zone: bool) -> &mut Self {
         self.has_quiet_zone = has_quiet_zone;
